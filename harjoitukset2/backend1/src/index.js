6c7dd103e703fd01a71a43f6539d33579d613b85
jvwlehti <93128297+jvwlehti@users.noreply.github.com>
2022-01-20 13:28:51 +0200

    muutoksia

--- harjoitukset2/backend1/src/index.js
+++ harjoitukset2/backend1/src/index.js
@@ -4,7 +4,7 @@ import App from './App'
 import axios from 'axios'
 import './index.css'
 
-axios.get('https://afternoon-shore-76335.herokuapp.com/api/notes').then(response => {
+axios.get('/api/notes').then(response => {
   const notes = response.data
   ReactDOM.render(
     <App />, document.getElementById('root')
--- harjoitukset2/backend1/src/services/notes.js
+++ harjoitukset2/backend1/src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = 'https://afternoon-shore-76335.herokuapp.com/api/notes'
+const baseUrl = '/api/notes'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
